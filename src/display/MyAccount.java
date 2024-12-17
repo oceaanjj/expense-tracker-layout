60ab19e004fed341c4fc23d07f391155bb1f48b6
Jovilyn Esquerra <144090346+oceaanjj@users.noreply.github.com>
2024-12-17 16:29:32 +0800

    my account layouts

--- src/display/MyAccount.java
+++ src/display/MyAccount.java
@@ -2,11 +2,7 @@ package display;
 
 public class MyAccount extends menu {
     public void display() {
-        
-        System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n");
         System.out.println("\t\t\t\t\t\t\t╔═══════════════════════════════════════════════════════════════╗");
-        System.out.println("\t\t\t\t\t\t\t║                                                               ║");
-        System.out.println("\t\t\t\t\t\t\t║                          MY ACCOUNT                           ║");
         System.out.println("\t\t\t\t\t\t\t║                                                               ║");
         System.out.println("\t\t\t\t\t\t\t║                   [1] Change Email                            ║");
         System.out.println("\t\t\t\t\t\t\t║                   [2] Change Password                         ║");
@@ -18,4 +14,18 @@ public class MyAccount extends menu {
         System.out.println("\t\t\t\t\t\t\t║                                                               ║");
         System.out.println("\t\t\t\t\t\t\t╚═══════════════════════════════════════════════════════════════╝");
     }  
+    public void header(){
+        System.out.println("\n\n\n\n\n\n\n\n");
+        String yellow = "\u001B[33m";
+        String reset = "\u001B[0m";
+        String tabs = "\t\t\t\t\t\t\t"; 
+        System.out.println("\n\n");
+
+        System.out.println(yellow + tabs + "      __  ____   __     _    ____ ____ ___  _   _ _   _ _____ " + reset);
+        System.out.println(yellow + tabs + "     |  \\/  \\ \\ / /    / \\  / ___/ ___/ _ \\| | | | \\ | |_   _|" + reset);
+        System.out.println(yellow + tabs + "     | |\\/| |\\ V /    / _ \\| |  | |  | | | | | | |  \\| | | |  " + reset);
+        System.out.println(yellow + tabs + "     | |  | | | |    / ___ \\ |__| |__| |_| | |_| | |\\  | | |  " + reset);
+        System.out.println(yellow + tabs + "     |_|  |_| |_|   /_/   \\_\\____\\____\\___/ \\___/|_| \\_| |_|  " + reset);
+    }
+    
 }
