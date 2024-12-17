1aaebb345632fded10c7b3243379eb0515719cb7
Jovilyn Esquerra <144090346+oceaanjj@users.noreply.github.com>
2024-12-17 20:00:28 +0800

    fixing

--- src/display/warning.java
+++ src/display/warning.java
@@ -6,7 +6,7 @@ public class warning extends menu {
         String reset = "\u001B[0m";
         String tabs = "\t\t\t\t\t\t\t\t"; // 8 tabs
 
-        System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n");
+        System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
         System.out.println(red + tabs + "██╗" + reset);
         System.out.println(red + tabs + "██║" + reset);
         System.out.println(red + tabs + "██║" + reset);
